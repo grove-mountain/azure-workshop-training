1a7b666c42626515cc1e6435f79aacdba2c44835
Fraser Pollock <fraserpol@gmail.com>
2018-01-02 13:38:37 -0500

    Adding Chapter 2

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
 provider "azurerm" {
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 }
 
 resource "azurerm_resource_group" "myfirstrg" {
-  name     = "myfirstresourcegroup"
-  location = "East US"
+  name     = "${var.resource_group_name}"
+  location = "${var.resource_group_location}"
 
   tags {
-  environment = "Production"
+    environment = "${var.resource_group_tag}"
   }
+}
+
+resource "azurerm_virtual_network" "myfirstvn" {
+  name                = "${var.vn_name}"
+  address_space       = "${var.vn_address_space}"
+  location            = "${var.vn_location}"
+  resource_group_name = "${var.resource_group_name}"
 }
---
+++ variables.tf
@@ -0,0 +1,36 @@
+
+variable "resource_group_name" {
+  type = "string"
+  default = "myfirstresourcegroup"
+  description = "This variable defines the resource group name"
+}
+
+variable "resource_group_location" {
+  type = "string"
+  default = "East US"
+  description = "This variable defines the resource group location"
+}
+
+variable "resource_group_tag" {
+  type = "string"
+  default = "Production"
+  description = "A tag required by the company to identify the resource group"
+}
+
+variable "vn_name" {
+  type = "string"
+  default = "my_first_vn"
+  description = "This variable defines the virtual network name"
+}
+
+variable "vn_address_space" {
+  type = "list"
+  default = ["10.0.0.0/16"]
+  description = "This is the default open network"
+}
+
+variable "vn_location" {
+  type = "string"
+  default = "East US"
+  description = "This variable defines the virtual network location"
+}
