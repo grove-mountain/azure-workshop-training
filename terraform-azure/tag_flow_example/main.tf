4fa572be88965b07df764cda3bc79f4f7115b962
Jake Lundberg <grove.mountain.github@nym.hush.com>
2018-06-18 20:35:40 -0700

    jake_flow_tweaks: making easier to customize

--- terraform-azure/tag_flow_example/main.tf
+++ terraform-azure/tag_flow_example/main.tf
@@ -1,6 +1,6 @@
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "myfirstrg" {
-  name     = "${var.resource_group_name}"
+  name     = "${var.student}-ResourceGroup"
   location = "${var.resource_group_location}"
 
   tags {
